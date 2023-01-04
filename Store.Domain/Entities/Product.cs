e30d32ffe52aeef7cdb9407740c1a360e91b6fde
RichardKT88 <kendy_t88@hotmail.com>
2023-01-04 19:42:17 -0300

    Testando Queries - Todas as Aulas

--- Store.Domain/Entities/Product.cs
+++ Store.Domain/Entities/Product.cs
@@ -2,15 +2,15 @@ namespace Store.Domain.Entities
 {
     public class Product : Entity
     {
-        public Product(string? title, decimal price, bool activw)
+        public Product(string? title, decimal price, bool active)
         {
             Title = title;
             Price = price;
-            Activw = activw;
+            Active = active;
         }
 
         public string? Title { get; private set; }
         public decimal Price { get; private set; }
-        public bool Activw { get; private set; }
+        public bool Active { get; private set; }
     }
 }
