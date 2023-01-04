8cfd5c1d8227d7ba2fbf6446bf88ffe47bf7e63c
RichardKT88 <kendy_t88@hotmail.com>
2023-01-04 20:20:14 -0300

    Commands - Todas as Aulas

---
+++ Store.Domain/Commands/CreateOrderCommand.cs
@@ -0,0 +1,37 @@
+using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Interfaces;
+using Store.Domain.Entities;
+
+namespace Store.Domain.Commands
+{
+    public class CreateOrderCommand : Notifiable<Notification>, ICommand
+    {
+        public CreateOrderCommand()
+        {
+            Items = new List<CreateOrderItemCommand>();
+        }
+
+        public CreateOrderCommand(string customer, string zipCode, string promoCode, IList<CreateOrderItemCommand> items)
+        {
+            Customer = customer;
+            ZipCode = zipCode;
+            PromoCode = promoCode;
+            Items = items;
+        }
+
+        public string? Customer { get; set; }
+        public string? ZipCode { get; set; }
+        public string? PromoCode { get; set; }
+        public IList<CreateOrderItemCommand> Items { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract<Order>()
+               .Requires()
+               .IsGreaterThan(Customer, 11, "Customer", "Cliente inválido")
+               .IsGreaterThan(ZipCode, 8, "ZipCode", "CEP inválido")
+           );
+        }
+    }
+}
